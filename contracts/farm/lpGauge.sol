876049fcf83afeaee83ad59c4e76ef3faf240495
JupiterOrSaturn <JupiterOrSaturn@outlook.com>
2022-03-15 11:27:07 +0800

    create hexagon finance farm

---
+++ contracts/farm/lpGauge.sol
@@ -0,0 +1,125 @@
+/**
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+pragma solidity 0.6.12;
+import "@boringcrypto/boring-solidity/contracts/BoringOwnable.sol";
+import "../interfaces/IMiniChefV2.sol";
+contract lpGauge is BoringOwnable{
+    modifier notZeroAddress(address inputAddress) {
+        require(inputAddress != address(0), "Coin : input zero address");
+        _;
+    }
+
+    // --- ERC20 Data ---
+    // The name of this coin
+    string  public name;
+    // The symbol of this coin
+    string  public symbol;
+    // The version of this Coin contract
+    string  public constant version = "1";
+    // The number of decimals that this coin has
+    uint8   public constant decimals = 18;
+    // The total supply of this coin
+    uint256 public totalSupply;
+    uint256 public pid;
+
+    // Mapping of coin balances
+    mapping (address => uint256)                      public balanceOf;
+    // Mapping of allowances
+    mapping (address => mapping (address => uint256)) public allowance;
+    // Mapping of nonces used for permits
+    mapping (address => uint256)                      public nonces;
+
+    // --- Events ---
+    event AddAuthorization(address account);
+    event RemoveAuthorization(address account);
+    event Approval(address indexed src, address indexed guy, uint256 amount);
+    event Transfer(address indexed src, address indexed dst, uint256 amount);
+
+    // --- Math ---
+    function addition(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x + y) >= x, "lpGauge/add-overflow");
+    }
+    function subtract(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x - y) <= x, "lpGauge/sub-underflow");
+    }
+
+    constructor(string memory name_,
+        string memory symbol_,
+        uint256 pid_
+      )public {
+        name          = name_;
+        symbol        = symbol_;
+    }
+
+    // --- Token ---
+    /*
+    * @notice Transfer coins to another address
+    * @param dst The address to transfer coins to
+    * @param amount The amount of coins to transfer
+    */
+    function transfer(address dst, uint256 amount) external returns (bool) {
+        return transferFrom(msg.sender, dst, amount);
+    }
+    /*
+    * @notice Transfer coins from a source address to a destination address (if allowed)
+    * @param src The address from which to transfer coins
+    * @param dst The address that will receive the coins
+    * @param amount The amount of coins to transfer
+    */
+    function transferFrom(address src, address dst, uint256 amount)
+        public returns (bool)
+    {
+        require(dst != address(0), "Coin/null-dst");
+        require(dst != address(this), "Coin/dst-cannot-be-this-contract");
+        require(balanceOf[src] >= amount, "Coin/insufficient-balance");
+        if (src != msg.sender && allowance[src][msg.sender] != uint256(-1)) {
+            require(allowance[src][msg.sender] >= amount, "Coin/insufficient-allowance");
+            allowance[src][msg.sender] = subtract(allowance[src][msg.sender], amount);
+        }
+        balanceOf[src] = subtract(balanceOf[src], amount);
+        balanceOf[dst] = addition(balanceOf[dst], amount);
+        IMiniChefV2(owner).onTransfer(pid,src,dst);
+        emit Transfer(src, dst, amount);
+        return true;
+    }
+    /*
+    * @notice Mint new coins
+    * @param usr The address for which to mint coins
+    * @param amount The amount of coins to mint
+    */
+    function mint(address usr, uint256 amount) external onlyOwner notZeroAddress(usr) {
+        balanceOf[usr] = addition(balanceOf[usr], amount);
+        totalSupply    = addition(totalSupply, amount);
+        emit Transfer(address(0), usr, amount);
+    }
+    /*
+    * @notice Burn coins from an address
+    * @param usr The address that will have its coins burned
+    * @param amount The amount of coins to burn
+    */
+    function burn(address usr, uint256 amount) external {
+        require(balanceOf[usr] >= amount, "Coin/insufficient-balance");
+        if (owner != msg.sender && usr != msg.sender && allowance[usr][msg.sender] != uint256(-1)) {
+            require(allowance[usr][msg.sender] >= amount, "Coin/insufficient-allowance");
+            allowance[usr][msg.sender] = subtract(allowance[usr][msg.sender], amount);
+        }
+        balanceOf[usr] = subtract(balanceOf[usr], amount);
+        totalSupply    = subtract(totalSupply, amount);
+        if (owner != msg.sender){
+            IMiniChefV2(owner).onTransfer(pid,usr,address(0));
+        }
+        emit Transfer(usr, address(0), amount);
+    }
+    /*
+    * @notice Change the transfer/burn allowance that another address has on your behalf
+    * @param usr The address whose allowance is changed
+    * @param amount The new total allowance for the usr
+    */
+    function approve(address usr, uint256 amount) external notZeroAddress(usr) returns (bool)  {
+        allowance[msg.sender][usr] = amount;
+        emit Approval(msg.sender, usr, amount);
+        return true;
+    }
+
+}
