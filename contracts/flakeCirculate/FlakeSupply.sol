926ca1be918b7b9880af4f9bdfea33c0d5e1161d
hexgonjia <forestjia@yeah.net>
2022-04-21 21:06:55 -0700

    added circulate cal sc

--- contracts/flakeCirculate/FlakeSupply.sol
+++ contracts/flakeCirculate/FlakeSupply.sol
@@ -9,7 +9,9 @@ import "./IERC20.sol";
 contract FlakeSupply is Ownable{
     using SafeMath for uint256;
     address public tokenAddress;
-    address[] public fixedBalAddress;
+
+    address[] public lockedBalAddress;
+    address[] public lokcedBalAddress;
 
     constructor(address _tokenAddress) public {
         tokenAddress = _tokenAddress;
@@ -20,19 +22,27 @@ contract FlakeSupply is Ownable{
     }
 
     function addFixedAddress(address _fixedBalAddress) public onlyOwner{
-        fixedBalAddress.push(_fixedBalAddress);
+        lockedBalAddress.push(_fixedBalAddress);
     }
 
     function removeFixedAddress(address _fixedBalAddress) public onlyOwner{
-        for(uint256 i=0;i<fixedBalAddress.length;i++) {
-            if(fixedBalAddress[i] == _fixedBalAddress) {
-                fixedBalAddress[i] = address(0);
+        for(uint256 i=0;i< lockedBalAddress.length;i++) {
+            if(lockedBalAddress[i] == _fixedBalAddress) {
+                lockedBalAddress[i] = address(0);
             }
         }
     }
 
-    function getPriceTokenDecimal(address _tokenAddress) public view returns(uint256){
-        return (10**IERC20(_tokenAddress).decimals());
+    function addLockedAddress(address _lockedBalAddress) public onlyOwner{
+        lokcedBalAddress.push(_lockedBalAddress);
+    }
+
+    function removeLockeddAddress(address _lokcedBalAddress) public onlyOwner{
+        for(uint256 i=0;i<lokcedBalAddress.length;i++) {
+            if(lokcedBalAddress[i] == _lokcedBalAddress) {
+                lokcedBalAddress[i] = address(0);
+            }
+        }
     }
 
     function circulateSupply()
@@ -41,16 +51,47 @@ contract FlakeSupply is Ownable{
         returns (uint256)
     {
         uint256 fixedTotal = 0;
-        for(uint256 i=0;i<fixedBalAddress.length;i++) {
-            if(fixedBalAddress[i] != address (0)) {
-               uint256 bal = IERC20(tokenAddress).balanceOf(fixedBalAddress[i]);
+        for(uint256 i=0;i< lockedBalAddress.length;i++) {
+            if(lockedBalAddress[i] != address (0)) {
+               uint256 bal = IERC20(tokenAddress).balanceOf(lockedBalAddress[i]);
                fixedTotal = fixedTotal.add(bal);
             }
         }
 
         uint256 totalSupply = IERC20(tokenAddress).totalSupply();
-        uint256 decimal = getPriceTokenDecimal(tokenAddress);
-        return totalSupply.sub(fixedTotal).div(decimal);
+        return totalSupply.sub(fixedTotal);
     }
+
+
+    function lockedBalance()
+    public
+    view
+    returns (uint256)
+    {
+        uint256 lockedTotal = 0;
+        for(uint256 i=0;i< lockedBalAddress.length;i++) {
+            if(lockedBalAddress[i] != address (0)) {
+                uint256 bal = IERC20(tokenAddress).balanceOf(lockedBalAddress[i]);
+                lockedTotal = lockedTotal.add(bal);
+            }
+        }
+
+        return lockedTotal;
+    }
+
+    function lockedRatio()
+    public
+    view
+    returns (uint256) {
+        uint256 circulate = circulateSupply();
+
+        if(circulate>0) {
+          return lockedBalance().mul(1000).div(circulate);
+        } else {
+            return 0;
+        }
+    }
+
+
 
 }
--- contracts/mock/MockToken.sol
+++ contracts/mock/MockToken.sol
@@ -7,16 +7,24 @@ import "../libraries/Ownable.sol";
 
 contract MockToken is ERC20, Ownable {
     string public name;
-    uint256 public decimal;
+    uint256 public decimals_;
     string public symbol;
 
     constructor(string memory _name,string memory _symbol,uint256 _decimal) public {
         name = _name;
         symbol = _symbol;
-        decimal = _decimal;
+        decimals_ = _decimal;
     }
 
     function mint(address _to, uint256 _amount) public {
         _mint(_to, _amount);
     }
+
+    /**
+     * @return the number of decimals of the token.
+     */
+    function decimals() public view returns (uint8) {
+        return uint8(decimals_);
+    }
+
 }
