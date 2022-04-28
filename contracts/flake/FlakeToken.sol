8a04894a8a11ff36a0140a17e075175f555827cc
hexgonjia <forestjia@yeah.net>
2022-04-27 17:57:12 -0700

    changed addcording to audit

--- contracts/flake/FlakeToken.sol
+++ contracts/flake/FlakeToken.sol
@@ -4,20 +4,19 @@ pragma solidity 0.6.12;
 import './ERC20.sol';
 
 contract FlakeToken is ERC20{
-    using SafeMath for uint;
 
     string private name_;
     string private symbol_;
     uint8  private decimals_;
 
     //total tokens supply
-    uint256 constant public MAX_TOTAL_TOKEN_AMOUNT = 100000000 ether;
-    uint256 constant public MAX_RESERVE_AMOUNT = 10000000 ether;     //10% for reserve
-    uint256 constant public MAX_BUSINESS_EXPANDING = 5000000 ether;  //5% for business expanding
+    uint256 constant public MAX_TOTAL_TOKEN_AMOUNT = 100_000_000 ether;
+    uint256 constant public MAX_RESERVE_AMOUNT = 10_000_000 ether;     //10% for reserve
+    uint256 constant public MAX_BUSINESS_EXPANDING = 5_000_000 ether;  //5% for business expanding
 
     constructor(string memory tokenName,
                 string memory tokenSymbol,
-                uint256 tokenDecimal,
+                uint8 tokenDecimal,
                 address initHolder,
                 address reserveHolder,
                 address businessHolder)
@@ -25,7 +24,7 @@ contract FlakeToken is ERC20{
     {
         name_ = tokenName;
         symbol_ = tokenSymbol;
-        decimals_ = uint8(tokenDecimal);
+        decimals_ = tokenDecimal;
 
         _mint(reserveHolder,MAX_RESERVE_AMOUNT);
         _mint(businessHolder,MAX_BUSINESS_EXPANDING);
@@ -35,21 +34,21 @@ contract FlakeToken is ERC20{
     /**
      * @return the name of the token.
      */
-    function name() public view returns (string memory) {
+    function name() external view returns (string memory) {
         return name_;
     }
 
     /**
      * @return the symbol of the token.
      */
-    function symbol() public view returns (string memory) {
+    function symbol() external view returns (string memory) {
         return symbol_;
     }
 
     /**
      * @return the number of decimals of the token.
      */
-    function decimals() public view returns (uint8) {
+    function decimals() external view returns (uint8) {
         return decimals_;
     }
 
--- contracts/veFlake/veFlake.sol
+++ contracts/veFlake/veFlake.sol
@@ -3,15 +3,17 @@
 pragma solidity 0.6.12;
 import '../flake/ERC20.sol';
 import '../libraries/SafeMath.sol';
-import '../interfaces/IERC20.sol';
+import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
 
 contract veFlake is ERC20 {
     using SafeMath for uint256;
+    using BoringERC20 for IERC20;
+
     IERC20 immutable public flake;
     address public safeMulsig;
 
     modifier onlyOrigin() {
-        require(msg.sender==safeMulsig, "not mulsafe");
+        require(msg.sender==safeMulsig, "not mulsig address");
         _;
     }
 
@@ -25,27 +27,29 @@ contract veFlake is ERC20 {
     uint8  private decimals_;
 
     uint64 public LeavingTerm = 90 days;
+
     struct pendingItem {
         uint192 pendingAmount;
-        uint64 releaseTime;
+        uint64 timestamp;
     }
+
     struct pendingGroup {
-        pendingItem[] pendingAry;
+        pendingItem[] pendingArray;
         uint192 pendingDebt;
         uint64 firstIndex;
     }
 
     mapping(address=>pendingGroup) public userLeavePendingMap;
     // Define the token contract
-    constructor(IERC20 _flake,address _multiSignature,string memory tokenName,string memory tokenSymbol,uint256 tokenDecimal) public {
+    constructor(IERC20 _flake,address _multiSignature,string memory tokenName,string memory tokenSymbol,uint8 tokenDecimal) public {
         safeMulsig = _multiSignature;
         flake = _flake;
 
         name_ = tokenName;
         symbol_ = tokenSymbol;
-        decimals_ = uint8(tokenDecimal);
-
+        decimals_ = tokenDecimal;
     }
+
     function toUint192(uint256 a) internal pure returns (uint192 c) {
         require(a <= uint192(-1), "BoringMath: uint192 Overflow");
         c = uint192(a);
@@ -58,34 +62,35 @@ contract veFlake is ERC20 {
 //        LeavingTerm = _leavingTerm;
 //    }
 
-    function setMulsig(address _multiSignature) external onlyOrigin{
-        safeMulsig = _multiSignature;
-    }
+//    function setMulsig(address _multiSignature) external onlyOrigin{
+//        safeMulsig = _multiSignature;
+//    }
 
     /**
      * @return the name of the token.
      */
-    function name() public view returns (string memory) {
+    function name() external view returns (string memory) {
         return name_;
     }
 
     /**
      * @return the symbol of the token.
      */
-    function symbol() public view returns (string memory) {
+    function symbol() external view returns (string memory) {
         return symbol_;
     }
 
     /**
      * @return the number of decimals of the token.
      */
-    function decimals() public view returns (uint8) {
+    function decimals() external view returns (uint8) {
         return decimals_;
     }
 
 
-    function enter(uint256 _amount) public {
-        // Gets the amount of locked in the contract
+    function enter(uint256 _amount) external {
+        require(_amount>0,"amount need to over zero!");
+        // Gets the amount of locked flake in the contract
         uint256 totalFlake = flake.balanceOf(address(this));
         // Gets the amount of veFlake in existence
         uint256 totalShares = totalSupply();
@@ -103,37 +108,40 @@ contract veFlake is ERC20 {
         }
 
         // Lock the flake in the contract
-        flake.transferFrom(msg.sender, address(this), _amount);
+        flake.safeTransferFrom(msg.sender, address(this), _amount);
 
 
     }
 
-    function leaveApply(uint256 _share) public {
+    function leaveApply(uint256 _share) external {
+        require(_share>0,"_share need to over zero!");
+
         addPendingInfo(userLeavePendingMap[msg.sender],_share);
         _transfer(msg.sender, address(this), _share);
         emit ApplyLeave(msg.sender, _share);
+      }
 
-        //require(getAllPendingAmount(userLeavePendingMap[msg.sender])>=_share,"veFlake: Leave insufficient amount");
-    }
-
-    function cancelLeave()public{
+    function cancelLeave() external {
         pendingGroup storage userPendings = userLeavePendingMap[msg.sender];
-        uint256 pendingLength = userPendings.pendingAry.length;
+        uint256 pendingLength = userPendings.pendingArray.length;
         require(pendingLength > 0,"veFlake : Empty leave pending queue!");
            // leave();
-        uint256 amount = userPendings.pendingAry[uint256(pendingLength-1)].pendingAmount - userPendings.pendingDebt;
+        uint256 amount = userPendings.pendingArray[uint256(pendingLength-1)].pendingAmount - userPendings.pendingDebt;
         _transfer(address(this),msg.sender,amount);
 
         userPendings.firstIndex = uint64(pendingLength);
-        userPendings.pendingDebt = userPendings.pendingAry[uint256(pendingLength-1)].pendingAmount;
+        userPendings.pendingDebt = userPendings.pendingArray[uint256(pendingLength-1)].pendingAmount;
         emit  CancelLeave(msg.sender,amount);
     }
     // Leave the bar. Claim back your flake.
     // Unlocks the staked + gained flake and burns veFlake
-    function leave() public {
+    function leave() external {
         // Gets the amount of veFlake in existence
         uint256 totalShares = totalSupply();
         uint256 _share = updateUserPending(userLeavePendingMap[msg.sender],LeavingTerm);
+
+        require(_share>0,"pending share need to be over 0!");
+
         // Calculates the amount of flake the veFlake is worth
         uint256 what = _share.mul(flake.balanceOf(address(this))).div(
             totalShares
@@ -141,26 +149,26 @@ contract veFlake is ERC20 {
 
         _burn(address(this), _share);
 
-        flake.transfer(msg.sender, what);
+        flake.safeTransfer(msg.sender, what);
 
         emit Leave(msg.sender, what,_share);
 
     }
 
-    function searchPendingIndex(pendingItem[] storage pendingAry,uint64 firstIndex,uint64 searchTime) internal view returns (int256){
-        uint256 length = pendingAry.length;
-        if (uint256(firstIndex)>=length || pendingAry[firstIndex].releaseTime > searchTime) {
+    function searchPendingIndex(pendingItem[] storage pendingArray,uint64 firstIndex,uint64 searchTime) internal view returns (int256){
+        uint256 length = pendingArray.length;
+        if (uint256(firstIndex)>=length || pendingArray[firstIndex].timestamp > searchTime) {
             return int256(firstIndex) - 1;
         }
         uint256 min = firstIndex;
         uint256 max = length - 1;
         while (max > min) {
             uint256 mid = (max + min + 1) / 2;
-            if (pendingAry[mid].releaseTime == searchTime) {
+            if (pendingArray[mid].timestamp == searchTime) {
                 min = mid;
                 break;
             }
-            if (pendingAry[mid].releaseTime < searchTime) {
+            if (pendingArray[mid].timestamp < searchTime) {
                 min = mid;
             } else {
                 max = mid - 1;
@@ -171,22 +179,22 @@ contract veFlake is ERC20 {
 
 
     function addPendingInfo(pendingGroup storage userPendings,uint256 amount) internal {
-        uint256 len = userPendings.pendingAry.length;
+        uint256 len = userPendings.pendingArray.length;
         if (len != 0){
             uint64 curTime = currentTime();
-            if (userPendings.pendingAry[len-1].releaseTime == curTime){
-                userPendings.pendingAry[len-1].pendingAmount= toUint192(amount.add(userPendings.pendingAry[len-1].pendingAmount));
+            if (userPendings.pendingArray[len-1].timestamp == curTime){
+                userPendings.pendingArray[len-1].pendingAmount= toUint192(amount.add(userPendings.pendingArray[len-1].pendingAmount));
             }else{
-                userPendings.pendingAry.push(pendingItem(toUint192(amount.add(userPendings.pendingAry[len-1].pendingAmount)),curTime));
+                userPendings.pendingArray.push(pendingItem(toUint192(amount.add(userPendings.pendingArray[len-1].pendingAmount)),curTime));
             }
         }else{
-            userPendings.pendingAry.push(pendingItem(toUint192(amount),currentTime()));
+            userPendings.pendingArray.push(pendingItem(toUint192(amount),currentTime()));
         }
     }
 
 
 
-    function getUserReleasePendingAmount(address account) public view returns (uint256){
+    function getUserReleasePendingAmount(address account) external view returns (uint256){
         return getReleasePendingAmount(userLeavePendingMap[account],LeavingTerm);
     }
 
@@ -195,23 +203,23 @@ contract veFlake is ERC20 {
     }
 
     function getAllPendingAmount(pendingGroup storage userPendings) internal view returns (uint256){
-        uint256 len = userPendings.pendingAry.length;
+        uint256 len = userPendings.pendingArray.length;
         if(len == 0){
             return 0;
         }
-        return SafeMath.sub(userPendings.pendingAry[len-1].pendingAmount,userPendings.pendingDebt);
+        return SafeMath.sub(userPendings.pendingArray[len-1].pendingAmount,userPendings.pendingDebt);
     }
 
     function getReleasePendingAmount(pendingGroup storage userPendings,uint64 releaseTerm) internal view returns (uint256){
         uint64 curTime = uint64(SafeMath.sub(currentTime(),releaseTerm));
-        int256 index = searchPendingIndex(userPendings.pendingAry,userPendings.firstIndex,curTime);
+        int256 index = searchPendingIndex(userPendings.pendingArray,userPendings.firstIndex,curTime);
         if (index<int256(userPendings.firstIndex)){
             return 0;
         }
-        return SafeMath.sub(userPendings.pendingAry[uint256(index)].pendingAmount,userPendings.pendingDebt);
+        return SafeMath.sub(userPendings.pendingArray[uint256(index)].pendingAmount,userPendings.pendingDebt);
     }
 
-    function getFlakeAmount(uint256 _share) public view returns (uint256) {
+    function getFlakeAmount(uint256 _share) external view returns (uint256) {
         // Gets the amount of veFlake in existence
         uint256 totalShares = totalSupply();
         if(totalShares==0) {
@@ -222,7 +230,7 @@ contract veFlake is ERC20 {
 
     }
 
-    function getVeFlakeShare(uint256 _amount) public view returns (uint256) {
+    function getVeFlakeShare(uint256 _amount) external view returns (uint256) {
         uint256 totalFlake = flake.balanceOf(address(this));
         // Gets the amount of veFlake in existence
         uint256 totalShares = totalSupply();
@@ -230,8 +238,7 @@ contract veFlake is ERC20 {
         if (totalShares == 0 || totalFlake == 0) {
             return _amount;
         }
-        // Calculate and mint the amount of veFlake the flake is worth. The ratio will change overtime, as veFlake is burned/minted and flake deposited + gained from fees / withdrawn.
-        else {
+         else {
             return _amount.mul(totalShares).div(totalFlake);
         }
     }
@@ -240,13 +247,13 @@ contract veFlake is ERC20 {
     function updateUserPending(pendingGroup storage userPendings,uint64 releaseTerm)internal returns (uint256){
 
         uint64 curTime = uint64(SafeMath.sub(currentTime(),releaseTerm));
-        int256 index = searchPendingIndex(userPendings.pendingAry,userPendings.firstIndex,curTime);
+        int256 index = searchPendingIndex(userPendings.pendingArray,userPendings.firstIndex,curTime);
         if (index<int256(userPendings.firstIndex)){
             return 0;
         }
         userPendings.firstIndex = uint64(index + 1);
-        uint256 amount = SafeMath.sub(userPendings.pendingAry[uint256(index)].pendingAmount,userPendings.pendingDebt);
-        userPendings.pendingDebt = userPendings.pendingAry[uint256(index)].pendingAmount;
+        uint256 amount = SafeMath.sub(userPendings.pendingArray[uint256(index)].pendingAmount,userPendings.pendingDebt);
+        userPendings.pendingDebt = userPendings.pendingArray[uint256(index)].pendingAmount;
         return amount;
     }
 
@@ -259,18 +266,18 @@ contract veFlake is ERC20 {
         pendingGroup memory userPendings = userLeavePendingMap[account];
         uint256 firstIndex = userPendings.firstIndex;
 
-        uint256 len = userPendings.pendingAry.length - userPendings.firstIndex;
+        uint256 len = userPendings.pendingArray.length - userPendings.firstIndex;
         uint256[] memory amounts = new uint256[](len);
         uint256[] memory timeStamps = new uint256[](len);
 
-        for(uint256 i=firstIndex;i<userPendings.pendingAry.length;i++) {
+        for(uint256 i=firstIndex;i<userPendings.pendingArray.length;i++) {
             uint256 idx = i-firstIndex;
-            timeStamps[idx] = userPendings.pendingAry[i].releaseTime;
+            timeStamps[idx] = userPendings.pendingArray[i].timestamp;
 
             if(i==0) {
-                amounts[idx] = userPendings.pendingAry[i].pendingAmount;
+                amounts[idx] = userPendings.pendingArray[i].pendingAmount;
             } else {
-                amounts[idx] = userPendings.pendingAry[i].pendingAmount - userPendings.pendingAry[i-1].pendingAmount;
+                amounts[idx] = userPendings.pendingArray[i].pendingAmount - userPendings.pendingArray[i-1].pendingAmount;
             }
         }
 
