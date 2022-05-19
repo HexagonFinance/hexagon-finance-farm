92d30de1d0976703246f6e7920f34c296339253f
hexgonjia <forestjia@yeah.net>
2022-05-18 17:34:02 -0700

    rename files and rm unused files

--- contracts/rewarder/IMiniChefPool.sol
+++
@@ -1,9 +0,0 @@
-/**
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
-pragma solidity 0.6.12;
-import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
-interface IMiniChefPool {
-    function lpToken(uint256 pid) external view returns (IERC20 _lpToken);
-    function lpGauges(uint256 pid) external view returns (IERC20 _lpGauge);
-}
